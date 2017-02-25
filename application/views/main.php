2e076362a5649c09ea1302fa156f620d5babc00a
Marge Seppo <margeseppo@gmail.com>
2017-02-25 22:32:37 +0200

    Layout of the home page (draft), see #25

--- application/views/main.php
+++ application/views/main.php
@@ -2,84 +2,66 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-<div class="container">
-    <h1>Lorem Ipsum</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
+<div class="container-fluid">
+    <div class="row content">
+        <div class="col-sm-3 sidenav">
+            <img src="/images/girlr.jpg" class="img-responsive" alt="Children" width=100% height=100%>
+        </div>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
+        <div class="col-sm-9">
+            <h1>KIDS SAY</h1>
+            <h3><small>RECENT POSTS</small></h3>
+            <hr>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
+            <blockquote class="blockquote">
+                <p class="mb-0"
+                   title="Allikas: http://www.huffingtonpost.com/2015/05/06/the-funniest-thing-your-kid-said_n_7213254.html">
+                    Mary: "Can I have a Twik?" Me: "You mean Twix?" Mary: "No. I only want one."</p>
+                <footer class="blockquote-footer"> Mary, 5</footer>
+            </blockquote>
+            <hr>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
+            <blockquote class="blockquote">
+                <p class="mb-0"
+                   title="Allikas: http://thoughtcatalog.com/charlie-shaw/2014/04/32-people-share-the-funniest-thing-theyve-heard-a-kid-say/">
+                    Son and I are playing catch. I have a terrible throw that sails over his head. I say,
+                    “Sorry, that was a bad throw.” He stops, gives me a kind look and says, “No daddy, that was a
+                    wonderful throw.” Then takes 2 steps towards getting the ball. He stops again, turns back around
+                    and says, “When we say something nice, even when we don’t mean it at all, that’s called being
+                    polite, right?” </p>
+                <footer class="blockquote-footer"> Roberto, 6</footer>
+            </blockquote>
+            <hr>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
+            <blockquote class="blockquote">
+                <p class="mb-0"
+                   title="Allikas: http://thoughtcatalog.com/charlie-shaw/2014/04/32-people-share-the-funniest-thing-theyve-heard-a-kid-say/">
+                    While driving on the interstate with my 2.5 year old in the back seat: “Any idiots out today, dad?”</p>
+                <footer class="blockquote-footer"> John, 2</footer>
+            </blockquote>
+            <hr>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
+            <blockquote class="blockquote">
+                <p class="mb-0"
+                   title="Allikas: http://thoughtcatalog.com/charlie-shaw/2014/04/32-people-share-the-funniest-thing-theyve-heard-a-kid-say/">
+                    My Son: “Dad, can we get a cat?” Me: “Your Mom is allergic to cats, so no.”
+                    My Son: “When Mom dies can we get a cat?” Me: “Sure.”</p>
+                <footer class="blockquote-footer"> James, 6</footer>
+            </blockquote>
+            <hr>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
+            <blockquote class="blockquote">
+                <p class="mb-0"
+                   title="Allikas: http://thoughtcatalog.com/charlie-shaw/2014/04/32-people-share-the-funniest-thing-theyve-heard-a-kid-say/">
+                    I try to make it a point to take about 10 min at the end of each evening to
+                    help my daughter straighten up her room. After a particularly rough day, I sat down on the floor
+                    of her room to help her sort thru her dolls and I had muttered “I really don’t have the juice for
+                    this tonight…”. My daughter left the room and returned a few minutes later with a cup of apple
+                    juice for me.</p>
+                <footer class="blockquote-footer"> Jane, 7</footer>
+            </blockquote>
+            <hr>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
+        </div>
+    </div>
 </div>
