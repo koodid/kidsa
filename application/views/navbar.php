9e79eb0096fe217f701ac98f02c111770157aca4
Marge Seppo <margeseppo@gmail.com>
2017-02-26 20:00:28 +0200

    All tooltips in the same style, see #7

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -51,7 +51,9 @@ function echoActiveClassIfRequestMatches($requestUri)
                            data-toggle="tooltip" data-placement="bottom"
                            title="<?php echo lang("msg_search_tooltip_long"); ?>">
                     <div class="input-group-btn">
-                        <button class="btn btn-default" type="submit" title="<?php echo lang("msg_search_tooltip"); ?>">
+                        <button class="btn btn-default" type="submit"
+                                data-toggle="tooltip" data-placement="bottom"
+                                title="<?php echo lang("msg_search_tooltip"); ?>">
                             <i class="glyphicon glyphicon-search"></i>
                         </button>
                     </div>
@@ -60,17 +62,20 @@ function echoActiveClassIfRequestMatches($requestUri)
             <ul class="nav navbar-nav navbar-right">
                 <li>
                     <a href="<?php echo base_url(); ?>langswitch/switchLanguage/english?uri=<?php echo $_SERVER['REQUEST_URI']; ?>"
+                       data-toggle="tooltip" data-placement="bottom"
                        title="<?php echo lang("msg_english"); ?>">English</a>
                 </li>
                 <li>
                     <a href="<?php echo base_url(); ?>langswitch/switchLanguage/estonian?uri=<?php echo $_SERVER['REQUEST_URI']; ?>"
+                       data-toggle="tooltip" data-placement="bottom"
                        title="<?php echo lang("msg_estonian"); ?>">Eesti</a>
                 </li>
-                <li><a href="#" title="<?php echo lang("msg_login"); ?>"><span
-                                class="glyphicon glyphicon-log-in"></span>
+                <li><a href="#" data-toggle="tooltip" data-placement="bottom"
+                       title="<?php echo lang("msg_login"); ?>"><span class="glyphicon glyphicon-log-in"></span>
                         <?php echo lang("msg_login"); ?></a></li>
                 <li <?php echoActiveClassIfRequestMatches("signup") ?>>
-                    <a href="/signup" title="<?php echo lang("msg_signup"); ?>">
+                    <a href="/signup" data-toggle="tooltip" data-placement="bottom"
+                       title="<?php echo lang("msg_signup"); ?>">
                         <span class="glyphicon glyphicon-user"></span>
                         <?php echo lang("msg_signup"); ?>
                     </a>
