46c7ec5b81ce4aa5e9b39fb21f2cf1067457b264
Marge Seppo <margeseppo@gmail.com>
2017-02-26 17:31:56 +0200

    Layout of About page (draft), see #25

--- application/views/about.php
+++ application/views/about.php
@@ -2,16 +2,24 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-<div class="container">
-    <h1>About KIDSA</h1>
+<div class="container-fluid">
+    <div class="row content">
+        <div class="col-sm-3 sidenav">
+            <img src="/images/children-p.jpg" class="img-responsive" alt="Children" width=100% height=100%>
+        </div>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
+        <div class="col-sm-9">
+            <h1>About KIDSA</h1>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
-        in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
-        vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
-        Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
+                in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
+                vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
+                Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada mauris,
+                in ullamcorper sapien pulvinar ac. Vivamus sagittis vulputate congue. Maecenas fringilla
+                vitae odio ac porttitor. In hac habitasse platea dictumst. Nam pellentesque nec diam a maximus.
+                Nam sit amet lorem egestas, tincidunt augue a, tempus nunc. Mauris sed aliquam magna. </p>
+        </div>
+    </div>
 </div>
