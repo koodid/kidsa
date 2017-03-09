fdb7d9f6019c4869a6c19def15cca10a7f00003e
kivimats <matskivistik@gmail.com>
2017-03-09 15:35:47 +0200

    Created new procedures for adding users and FBusers.. and removed INSERT queries

--- application/models/Register_model.php
+++ application/models/Register_model.php
@@ -10,13 +10,11 @@ class Register_model extends CI_Model
 
     public function create_new_user()
     {
-        $name = $this->db->escape($this->input->post("name"));
         $username = $this->db->escape($this->input->post("username"));
         $password = password_hash($this->input->post("password"), PASSWORD_DEFAULT);
         $email = $this->db->escape($this->input->post("email"));
 
-        //$sql = "CALL addNewUser($name, $username, '$password', $email)";
-        $sql = "INSERT INTO users (Username, Password, Email) VALUES ($username, '$password', $email)";
+        $sql = "CALL createNewUser($username, '$password', $email)";
         $this->db->query($sql);
     }
 
@@ -24,12 +22,9 @@ class Register_model extends CI_Model
     {
         $name = $this->db->escape($name);
         $username = $this->db->escape($fb_id);
-        $password = "";
         $email = $this->db->escape($email);
-
-        //$sql = "CALL addNewUser($name, $username, '$password', $email)";'
-        $sql = "INSERT INTO users (Name, Username, Password, Email) VALUES ($name, $username, '$password', $email)";
-
+        
+        $sql = "CALL createNewFBuser($name, $username, $email)";
         $this->db->query($sql);
     }
 }
