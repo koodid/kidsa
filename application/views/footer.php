1ce183435b0132bdb3d621aed79f4e1b2478c56e
Marge Seppo <margeseppo@gmail.com>
2017-03-09 16:07:35 +0200

    Fix: The contrast between the colour of text and its background for the element is not sufficient to meet WCAG2.0 Level AA , see #26

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <footer class="footer">
     <div class="container">
-        <p class="text-muted">&copy; Copyright 2017</p>
+        <p><small>&copy; Copyright 2017</small></p>
     </div>
 </footer>
 
