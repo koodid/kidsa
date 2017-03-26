74b68c99ff9a3d0da17c5770384d7518e0fe2eae
Marge Seppo <margeseppo@gmail.com>
2017-03-26 23:11:48 +0300

    remove static data from the main page

--- application/views/main.php
+++ application/views/main.php
@@ -27,59 +27,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </div>
             </div>
             <button class="btn btn-default" id="load_post_button">Load more posts</button>
-            <hr>
-            <hr>
-
-            <blockquote class="blockquote">
-                <p class="mb-0"
-                   title="Allikas: http://www.huffingtonpost.com/2015/05/06/the-funniest-thing-your-kid-said_n_7213254.html">
-                    Mary: "Can I have a Twik?" Me: "You mean Twix?" Mary: "No. I only want one."</p>
-                <footer class="blockquote-footer"> Mary, 5</footer>
-            </blockquote>
-            <hr>
-
-            <blockquote class="blockquote">
-                <p class="mb-0"
-                   title="Allikas: http://thoughtcatalog.com/charlie-shaw/2014/04/32-people-share-the-funniest-thing-theyve-heard-a-kid-say/">
-                    Son and I are playing catch. I have a terrible throw that sails over his head. I say,
-                    “Sorry, that was a bad throw.” He stops, gives me a kind look and says, “No daddy, that was a
-                    wonderful throw.” Then takes 2 steps towards getting the ball. He stops again, turns back around
-                    and says, “When we say something nice, even when we don’t mean it at all, that’s called being
-                    polite, right?” </p>
-                <footer class="blockquote-footer"> Roberto, 6</footer>
-            </blockquote>
-            <hr>
-
-            <blockquote class="blockquote">
-                <p class="mb-0"
-                   title="Allikas: http://thoughtcatalog.com/charlie-shaw/2014/04/32-people-share-the-funniest-thing-theyve-heard-a-kid-say/">
-                    While driving on the interstate with my 2.5 year old in the back seat: “Any idiots out today,
-                    dad?”</p>
-                <footer class="blockquote-footer"> John, 2</footer>
-            </blockquote>
-            <hr>
-
-            <blockquote class="blockquote">
-                <p class="mb-0"
-                   title="Allikas: http://thoughtcatalog.com/charlie-shaw/2014/04/32-people-share-the-funniest-thing-theyve-heard-a-kid-say/">
-                    My Son: “Dad, can we get a cat?” Me: “Your Mom is allergic to cats, so no.”
-                    My Son: “When Mom dies can we get a cat?” Me: “Sure.”</p>
-                <footer class="blockquote-footer"> James, 6</footer>
-            </blockquote>
-            <hr>
-
-            <blockquote class="blockquote">
-                <p class="mb-0"
-                   title="Allikas: http://thoughtcatalog.com/charlie-shaw/2014/04/32-people-share-the-funniest-thing-theyve-heard-a-kid-say/">
-                    I try to make it a point to take about 10 min at the end of each evening to
-                    help my daughter straighten up her room. After a particularly rough day, I sat down on the floor
-                    of her room to help her sort thru her dolls and I had muttered “I really don’t have the juice for
-                    this tonight…”. My daughter left the room and returned a few minutes later with a cup of apple
-                    juice for me.</p>
-                <footer class="blockquote-footer"> Jane, 7</footer>
-            </blockquote>
-            <hr>
-
         </div>
     </div>
 </div>
