4f59bc638080bed90dbf9bc1c9e830f685b570b7
ProditorMagnus <public.ravana@gmail.com>
2017-05-07 22:38:51 +0300

    basic support of <br>

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -66,7 +66,7 @@
         <div class="col-md-8">
             <?php foreach ($posts as $post): ?>
                 <blockquote class="blockquote">
-                    <?php echo html_escape($post['Text']); ?>
+                    <?php echo str_replace("&lt;br&gt;", "<br>", html_escape($post['Text'])); ?>
                     <footer class="blockquote-footer"><?php echo html_escape($post['Name']); ?>
                         , <?php echo html_escape($post['Age']); ?></footer>
                 </blockquote>
